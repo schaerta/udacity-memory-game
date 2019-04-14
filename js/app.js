eb05448f14393f656d70ca730fbeda0bff5934f8
Tatiana Schär <tsc@spacesquad.de>
2019-04-14 14:05:16 +0200

    Added basic functionality - Checking for match now works properly - Cards open and close when appropriate

--- js/app.js
+++ js/app.js
@@ -4,21 +4,23 @@
 
 let container = document.querySelector('.container');
 let board = document.createElement('ul');
-board.classList.add("deck");
+board.classList.add('deck');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle (array) {
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
 /* Create Cards and add them to the board */
@@ -26,33 +28,34 @@ function shuffle(array) {
 const fragment = document.createDocumentFragment();
 cards = [];
 
-for (let i=1; i<17; i++) {
-  let card = document.createElement('li');
-  card.classList.add('card');
-  card.addEventListener('click', open);
-  let text = document.createElement('i');
-  text.classList.add('fa');
+for (let i = 1; i < 17; i++) {
+	let card = document.createElement('li');
+	card.classList.add('card');
+	card.isOpen = false;
+	card.addEventListener('click', open);
+	let text = document.createElement('i');
+	text.classList.add('fa');
 
-    if (i<3) {
-      text.classList.add('fa-diamond');
-    } else if (i<5) {
-      text.classList.add('fa-paper-plane-o');
-    } else if (i<7) {
-      text.classList.add('fa-anchor');
-    } else if (i<9) {
-      text.classList.add('fa-bolt');
-    } else if (i<11) {
-      text.classList.add('fa-leaf');
-    } else if (i<13) {
-      text.classList.add('fa-bicycle');
-    } else if (i<15) {
-      text.classList.add('fa-bomb');
-    } else {
-      text.classList.add('fa-cube');
-    }
+	if (i < 3) {
+		text.classList.add('fa-diamond');
+	} else if (i < 5) {
+		text.classList.add('fa-paper-plane-o');
+	} else if (i < 7) {
+		text.classList.add('fa-anchor');
+	} else if (i < 9) {
+		text.classList.add('fa-bolt');
+	} else if (i < 11) {
+		text.classList.add('fa-leaf');
+	} else if (i < 13) {
+		text.classList.add('fa-bicycle');
+	} else if (i < 15) {
+		text.classList.add('fa-bomb');
+	} else {
+		text.classList.add('fa-cube');
+	}
 
-  card.appendChild(text);
-  cards.push(card);
+	card.appendChild(text);
+	cards.push(card);
 
 }
 
@@ -60,14 +63,13 @@ for (let i=1; i<17; i++) {
 
 let newSet = shuffle(cards);
 
-for (let i=0; i<newSet.length; i++) {
-  board.appendChild(newSet[i]);
+for (let i = 0; i < newSet.length; i++) {
+	board.appendChild(newSet[i]);
 }
 
 /* Append Board to Document */
 
 container.appendChild(board);
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -80,34 +82,54 @@ container.appendChild(board);
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+let card = {};
+let domCard = {};
 let moves = 0;
 let openCards = [];
+let checkingForMatch = false;
 
-function checkMatch(card) {
-  let symbol1 = openCards[0].querySelector('i').classList.item(1);
-  let symbol2 = openCards[1].querySelector('i').classList.item(1);
-  /*let class1 = symbol1.classList.item(0);*/
-  console.log(symbol1, symbol2);
-  if (symbol1 === symbol2) {
-    console.log("Match!");
-  } else {
-    console.log("No match!");
-  }
-  openCards.splice(0,2);
+function addToOpenCards(card) {
+	openCards.push(card);
+	if (openCards.length >= 2) {
+		checkingForMatch = true;
+		checkForMatch(openCards);
+	}
 }
 
-function checkCard(card) {
-  openCards.push(card);
-  console.log(openCards);
-  if (openCards.length > 1) {
-    checkMatch(card);
-  }
+function checkForMatch (openCards) {
+	console.log(openCards[0], openCards[1]);
+	// if match
+	if ((openCards[0].querySelector('i').classList.value) === (openCards[1].querySelector('i').classList.value)) {
+		console.log('Match');
+		openCards[0].removeEventListener('click', open);
+		openCards[1].removeEventListener('click', open);
+		openCards.length = 0;
+		checkingForMatch = false;
+	} else {
+		console.log("No Match");
+		openCards[0].isOpen = false;
+		openCards[1].isOpen = false;
+		setTimeout(closeCards, 500, openCards);
+	}
 }
 
-function open() {
-  let card = this;
-  this.classList.toggle('open');
-  this.classList.toggle('show');
-  /*this.classList.toggle('match');*/
-  checkCard(card);
+function closeCards (openCards) {
+	openCards[0].classList.toggle('show');
+	openCards[1].classList.toggle('show');
+	openCards.length = 0;
+	checkingForMatch = false;
+}
+
+/* First function to execute when a card is clicked */
+function open () {
+	if (!checkingForMatch) {
+		card = this;
+		card.classList.toggle('show');
+		card.isOpen = !card.isOpen;
+		if (card.isOpen) {
+			addToOpenCards(card);
+		} else {
+			openCards.length = 0;
+		}
+	}
 }
