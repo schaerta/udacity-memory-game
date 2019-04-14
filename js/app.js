e0b1c6a1bdae918143b3559a9f0e9f74c364a107
Tatiana Schär <tsc@spacesquad.de>
2019-04-14 14:47:22 +0200

    Implemented Star Rating, work in progress on New Game button

--- js/app.js
+++ js/app.js
@@ -1,76 +1,5 @@
 /* Memory Game */
 
-/* Create Game Board */
-
-let container = document.querySelector('.container');
-let board = document.createElement('ul');
-board.classList.add('deck');
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle (array) {
-	var currentIndex = array.length,
-		temporaryValue,
-		randomIndex;
-
-	while (currentIndex !== 0) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-
-	return array;
-}
-
-/* Create Cards and add them to the board */
-
-const fragment = document.createDocumentFragment();
-cards = [];
-
-for (let i = 1; i < 17; i++) {
-	let card = document.createElement('li');
-	card.classList.add('card');
-	card.isOpen = false;
-	card.addEventListener('click', open);
-	let text = document.createElement('i');
-	text.classList.add('fa');
-
-	if (i < 3) {
-		text.classList.add('fa-diamond');
-	} else if (i < 5) {
-		text.classList.add('fa-paper-plane-o');
-	} else if (i < 7) {
-		text.classList.add('fa-anchor');
-	} else if (i < 9) {
-		text.classList.add('fa-bolt');
-	} else if (i < 11) {
-		text.classList.add('fa-leaf');
-	} else if (i < 13) {
-		text.classList.add('fa-bicycle');
-	} else if (i < 15) {
-		text.classList.add('fa-bomb');
-	} else {
-		text.classList.add('fa-cube');
-	}
-
-	card.appendChild(text);
-	cards.push(card);
-
-}
-
-/* Shuffle the cards and append them to board*/
-
-let newSet = shuffle(cards);
-
-for (let i = 0; i < newSet.length; i++) {
-	board.appendChild(newSet[i]);
-}
-
-/* Append Board to Document */
-
-container.appendChild(board);
-
 /*
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
@@ -78,16 +7,104 @@ container.appendChild(board);
 
 let numberOfMoves = 0;
 let moveDisplay = document.querySelector('.moves');
+let stars = document.querySelector('.stars');
 let card = {};
 let openCards = [];
 let matchedPairs = 0;
 let checkingForMatch = false;
 
+function startGame() {
+	/* Create Game Board */
+
+	let container = document.querySelector('.container');
+	let board = document.createElement('ul');
+	board.classList.add('deck');
+	let restartButton = document.querySelector('.restart');
+	restartButton.addEventListener('click', newGame);
+
+	// Shuffle function from http://stackoverflow.com/a/2450976
+	function shuffle (array) {
+		var currentIndex = array.length,
+			temporaryValue,
+			randomIndex;
+
+		while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	}
+
+	/* Create Cards and add them to the board */
+
+	const fragment = document.createDocumentFragment();
+	cards = [];
+
+	for (let i = 1; i < 17; i++) {
+		let card = document.createElement('li');
+		card.classList.add('card');
+		card.isOpen = false;
+		card.addEventListener('click', open);
+		let text = document.createElement('i');
+		text.classList.add('fa');
+
+		if (i < 3) {
+			text.classList.add('fa-diamond');
+		} else if (i < 5) {
+			text.classList.add('fa-paper-plane-o');
+		} else if (i < 7) {
+			text.classList.add('fa-anchor');
+		} else if (i < 9) {
+			text.classList.add('fa-bolt');
+		} else if (i < 11) {
+			text.classList.add('fa-leaf');
+		} else if (i < 13) {
+			text.classList.add('fa-bicycle');
+		} else if (i < 15) {
+			text.classList.add('fa-bomb');
+		} else {
+			text.classList.add('fa-cube');
+		}
+
+		card.appendChild(text);
+		cards.push(card);
+	}
+
+	/* Shuffle the cards and append them to board*/
+
+	let newSet = shuffle(cards);
+
+	for (let i = 0; i < newSet.length; i++) {
+		board.appendChild(newSet[i]);
+	}
+
+	/* Append Board to Document */
+
+	container.appendChild(board);
+
+	function newGame() {
+		board.remove();
+		startGame();
+	}
+}
+
 function addToOpenCards(card) {
 	openCards.push(card);
 	if (openCards.length >= 2) {
+		// Update Move Counter and Star Rating
 		numberOfMoves++;
 		moveDisplay.innerHTML = numberOfMoves;
+		if (numberOfMoves === 10) {
+			stars.firstElementChild.remove();
+		} else if(numberOfMoves === 20) {
+			stars.firstElementChild.remove();
+		} else if (numberOfMoves === 30) {
+			stars.firstElementChild.remove();
+		}
 		checkingForMatch = true;
 		checkForMatch(openCards);
 	}
@@ -138,3 +155,6 @@ function open () {
 		}
 	}
 }
+
+// Run function on page load
+startGame();
